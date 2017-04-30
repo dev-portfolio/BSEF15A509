8943ac8c7d7bcaef17ec865d56d900f821ef590f
mianjee112 <talharamzan704@gmail.com>
2017-04-30 22:01:44 +0500

    Add files via upload

---
+++ fcfs.py
@@ -0,0 +1,29 @@
+#!user/bin/python
+p={1 : [0,5] , 2 : [3,6],3 : [4,7]}   
+#p = []
+total=0
+2_time=[]
+1_time=[]
+#1_time=[0],2_time=[0]
+#n= input(" enter process number: ") 
+#for index in range (1,n):
+	#1_time= input("Enter Arrival time ")
+	#1_time.append(time)
+	#time1 = input("Enter Arrival time ")
+	#2_time.append(time1)
+	#p[index+1] = [1_time[index],2_time[index]]
+
+	#2_time[i]= input("Enter Burst time")
+#1_time.sort()
+#for i in range (1,n):
+	#print 1_time[i] , "  "
+
+
+for index in range (1,4):
+	
+	print (" Time of process " )
+	print total, "-----"a
+	b_time=p.get(index)[1]
+	total=total+2_time
+
+print total
